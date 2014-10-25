645e6c691b2425a93d897e1a575f90ffb8a40814
Razvan Marinescu <rvm10@imperial.ac.uk>
2014-10-25 19:27:47 +0100

    fixed the monte carlo bug, added more tests

--- 02_testing/monte_carlo.py
+++ 02_testing/monte_carlo.py
@@ -7,21 +7,36 @@ from mpl_toolkits.mplot3d import Axes3D
 
 def move_particle(particle_array):
 
+  particle_array = numpy.array(particle_array)
+
+  if particle_array.dtype.kind != 'i' and len(particle_array) > 0:
+    raise TypeError("particle_array should be an array of *integers*.")
+    # and the right values (positive or null)
+  if any(particle_array < 0):
+    raise ValueError("particle_array should be an array of *positive* integers.")
+  if particle_array.ndim != 1:
+    raise ValueError("particle_array should be an a *1-dimensional* array of positive integers.")
+  if not any(particle_array):
+    raise ValueError("particle array must contain at least one particle")
+
   n = len(particle_array)
+  print "n=",n
 
   selected_particle = int(rnd.uniform(0, n))
 
-  displacement = 0
+  displacement = -1
   if rnd.random() < 0.5:
     displacement = 1
 
   while (selected_particle + displacement < 0 or selected_particle + displacement >= n or particle_array[selected_particle] == 0):
     selected_particle = int(rnd.uniform(0, n))
 
-    displacement = 0
+    displacement = -1
     if rnd.random() < 0.5:
       displacement = 1
 
+  #print "selected_particle=", selected_particle
+  #print "displacement=", displacement
   particle_array[selected_particle] -= 1
   particle_array[selected_particle + displacement] += 1
 
@@ -48,19 +63,20 @@ def print_density_3d(densities):
 
   plt.show()
 
-def monte_carlo(density, temp):
+def monte_carlo(density, temp, iterations):
 
   n = len(density)
-  iterations = 30
-  densities = numpy.zeros(shape=(iterations+1,n))
+  densities = numpy.zeros(shape=(iterations+1,n), dtype=numpy.int8)
   densities[0] = density
   for i in range(iterations):
     energy_0 = energy(density)
 
     density_1 = move_particle(density)
-    energy_1 = energy(density)
+    energy_1 = energy(density_1)
 
-    if (energy_0 < energy_1):
+    print [energy_0, energy_1]
+    print density
+    if (energy_1 < energy_0):
       density = density_1
     else:
       p0 = math.exp(- (energy_1 - energy_0)/temp)
@@ -73,8 +89,12 @@ def monte_carlo(density, temp):
   return densities
 
 
-densities = monte_carlo([2,4,6,5,1,5], 10.0)
-print densities
+def main():
+  densities = monte_carlo([2,4,6,5,1,5], 10.0, 5)
+  #print densities
 
-print_density_3d(densities)
+  print_density_3d(densities)
 
+
+if __name__ == "__main__":
+    main()
---
+++ 02_testing/test_monte_carlo.py
@@ -0,0 +1,45 @@
+from diffusion_model import energy
+from nose.tools import assert_equal, assert_raises, assert_almost_equal
+import numpy as np
+from monte_carlo import move_particle, monte_carlo
+
+def test_move_particle_empty():
+  initial_array = [0,0,0]
+
+  with assert_raises(ValueError) as exception:    move_particle(initial_array)
+
+
+def test_move_particle_sum():
+  initial_array = [2,4,7]
+  sum_initial = sum(initial_array)
+  final_array = move_particle(initial_array)
+
+  assert_equal(sum(initial_array),sum(final_array))
+
+def test_move_particle_edge():
+  initial_array = [0,0,7]
+  final_array = move_particle(initial_array)
+
+  print final_array
+  assert_equal(any(final_array - [0,1,6]), False)
+
+def test_monte_carlo_zero_particles():
+  density = [0,0,0,0]
+  T = 2.0
+  iterations = 20
+
+  with assert_raises(ValueError) as exception:    monte_carlo(density, T, iterations)
+
+def test_monte_carlo_one_particle():
+  density = [0,0,1,0]
+  T = 2.0
+  iterations = 20
+  densities = monte_carlo(density, T, iterations)
+  constant_energy = energy(density)
+  print constant_energy
+
+  for i in range(len(density)):
+    print "energies:", [energy(densities[i]), constant_energy]
+    assert_equal(energy(densities[i]), constant_energy)
+
+

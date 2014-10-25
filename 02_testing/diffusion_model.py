f7c699e0f0032d4226dc2bd8e6b37d1496bc1c2a
Razvan Marinescu <rvm10@imperial.ac.uk>
2014-10-25 12:46:36 +0100

    finished implementing the basic version of monte carlo ... all particles tend to cluster into one cell

--- 02_testing/diffusion_model.py
+++ 02_testing/diffusion_model.py
@@ -13,16 +13,18 @@ def energy(density, coeff=1.0):
   """
   # implementation goes here
 
+  # Make sure input is an array
   density = array(density)
 
-  #for i in range(len(density) - 1 ):
-  #  energy += density[i] * (density[i]- 1)
+  print density
 
-
+  # of the right kind (integer). Unless it is zero length, in which case type does not matter.
   if density.dtype.kind != 'i' and len(density) > 0:
-    raise TypeError("Expected Array of integers");
-  if any(density) < 0:
-    raise ValueError("Expected array of *positive* integers")
-
+    raise TypeError("Density should be an array of *integers*.")
+  # and the right values (positive or null)
+  if any(density < 0):
+    raise ValueError("Density should be an array of *positive* integers.")
+  if density.ndim != 1:
+    raise ValueError("Density should be an a *1-dimensional* array of positive integers.")
 
   return sum(density * (density - 1)) * coeff * 0.5
--- 02_testing/monte_carlo.py
+++ 02_testing/monte_carlo.py
@@ -1,33 +1,44 @@
 from diffusion_model import energy
 import matplotlib.pyplot as plt
 import random as rnd
+import math
 
 def move_particle(particle_array):
-  
+
   n = len(particle_array)
-  selected_particle = round(rnd.uniform(0, n))
+
+  selected_particle = int(rnd.uniform(0, n))
 
   displacement = 0
   if rnd.random() < 0.5:
     displacement = 1
-    
+
+  while (selected_particle + displacement < 0 or selected_particle + displacement >= n or particle_array[selected_particle] == 0):
+    selected_particle = int(rnd.uniform(0, n))
+
+    displacement = 0
+    if rnd.random() < 0.5:
+      displacement = 1
+
   particle_array[selected_particle] -= 1
   particle_array[selected_particle + displacement] += 1
 
+  return particle_array
+
 def monte_carlo(density, temp):
 
-  iterations = 5
-  energy_0 = energy(density)
-
-  density_1 = move_particle(density)
-  energy_1 = energy(density)
-
+  iterations = 500
   for i in range(iterations):
+    energy_0 = energy(density)
+
+    density_1 = move_particle(density)
+    energy_1 = energy(density)
+
     if (energy_0 < energy_1):
       density = density_1
     else:
-      p0 = exp(- (energy_1 - energy_0)/temp)
-      p1 = random()
+      p0 = math.exp(- (energy_1 - energy_0)/temp)
+      p1 = rnd.random()
       if (p0 > p1):
         density = density_1
 

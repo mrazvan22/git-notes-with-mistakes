7571c5c2ce21c439deca5c4d37da1ad7679ff517
Razvan Marinescu <rvm10@imperial.ac.uk>
2014-11-23 14:06:27 +0000

    changed the yaml structure to add SI units, implemented addition and multiplication and added tests

--- 06_units/tests_units.py
+++ 06_units/tests_units.py
@@ -6,19 +6,45 @@ def test_building():
   two_cm = Unit('centimeters', 2)
   assert_equal(two_cm.value, 2)
   assert_equal(two_cm.unit, 'centimeters')
-  
+
   three_ms = Unit('milliseconds', 3)
   assert_equal(three_ms.value, 3)
   assert_equal(three_ms.unit, 'milliseconds')
 
-  with assert_raises(Exception) as e: 
+  with assert_raises(Exception) as e:
     Unit('pint_of_ale',1)
- 
+
 
 def test_conversion():
   two_cm = Unit('centimeters', 2)
   two_cm_in_dm = two_cm.to('decimeters')
   assert_almost_equal(two_cm_in_dm.value, 0.2)
   assert_equal(two_cm_in_dm.unit, 'decimeters')
-  
-       
+
+def test_add():
+  two_cm = Unit('centimeters', 2)
+  three_cm = Unit('centimeters', 3)
+  five_cm = two_cm + three_cm
+  assert_equal(five_cm.value, 5)
+  assert_equal(five_cm.unit, 'centimeters')
+
+  four_dm = Unit('decimeters', 4)
+  sum_m = two_cm + four_dm
+  assert_almost_equal(sum_m.value, 0.42)
+  assert_equal(sum_m.unit, 'meters')
+
+
+def test_mult():
+  two_cm = Unit('centimeters', 2)
+  three_cm = Unit('centimeters', 3)
+  six_cm = two_cm * three_cm
+  assert_equal(six_cm.value, 6)
+  assert_equal(six_cm.unit, 'centimeters')
+
+  four_dm = Unit('decimeters', 4)
+  mult_m = two_cm * four_dm
+  print mult_m.unit, mult_m.value
+  assert_almost_equal(mult_m.value, 0.008)
+  assert_equal(mult_m.unit, 'meters')
+
+
--- 06_units/units.py
+++ 06_units/units.py
@@ -4,14 +4,18 @@ class Unit(object):
 
   def load_units_from_config(filename):
     value_dict = {}
+    SI_dict = {}
     with open(filename, 'r') as f:
+      next(f)
       for line in f:
-        [key, value] = line.split(' ')
-        value_dict[key] = float(value[:-1])
+        print line
+        [key, value, SI_unit] = line.split(' ')
+        value_dict[key] = float(value)
+        SI_dict[key] = SI_unit[:-1]
 
-    return value_dict
+    return [value_dict, SI_dict]
 
-  value_map = load_units_from_config('units.yaml')
+  [value_map, SI_map] = load_units_from_config('units.yaml')
 
   def __init__(self, unit, value):
     if(unit in Unit.value_map.keys()):
@@ -20,24 +24,54 @@ class Unit(object):
     else:
       raise(Exception)
 
+  def get_SI_unit(self):
+    return Unit.SI_map[self.unit]
+
   def add(self, other_unit):
     new_unit = Unit(self.unit, self.value)
-    if (strcmp(other_unit.unit, self.unit) == 0):
+    if (other_unit.unit == self.unit):
       new_unit.value += other_unit.value
     else:
-      if (units_compatible(self.unit, other_unit.unit)):
-        new_unit.unit = get_SI_unit(self.unit)     
-        new_unit.value = self.to(new_unit.unit).value +  
-  
+      if (Unit.units_compatible(self.unit, other_unit.unit)):
+        new_unit.unit = self.get_SI_unit()
+        new_unit.value = self.to_SI().value + other_unit.to_SI().value
+      else:
+        raise(Exception)
+
     return new_unit
+
+  def multiply(self, other_unit):
+    new_unit = Unit(self.unit, self.value)
+    if (other_unit.unit == self.unit):
+      new_unit.value *= other_unit.value
+    else:
+      if (Unit.units_compatible(self.unit, other_unit.unit)):
+        new_unit.unit = self.get_SI_unit()
+        new_unit.value = self.to_SI().value * other_unit.to_SI().value
+      else:
+        raise(Exception)
+
+    return new_unit
+
+  def __add__(self, other):
+    return self.add(other)
+
+  def __mul__(self, other):
+    return self.multiply(other)
 
   def to(self, new_unit_label):
     if(new_unit_label in Unit.value_map.keys()):
       return Unit(new_unit_label, self.value * Unit.value_map[self.unit] / Unit.value_map[new_unit_label])
-    
+
+  def to_SI(self):
+    return self.to(self.get_SI_unit())
+
   def get_unit(self):
     return self.unit
 
+  @staticmethod
+  def units_compatible(unit1, unit2):
+    return (Unit.SI_map[unit1] == Unit.SI_map[unit2])
 
 
 class Time(Unit):
@@ -49,6 +83,7 @@ class Length(Unit):
     self.super(unit,value)
 
 
-  
-  
+
+#Unit.load_units_from_config('units.yaml')
+
 
